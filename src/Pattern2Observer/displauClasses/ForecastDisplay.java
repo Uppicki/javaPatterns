fbd1c25473f3308711e100a143f270966186f843
Your Name <vladimiruppicki@gmail.com>
2022-05-18 16:59:06 +0300

    ForecastDisplay was created

---
+++ src/Pattern2Observer/displauClasses/ForecastDisplay.java
@@ -0,0 +1,37 @@
+package Pattern2Observer.displauClasses;
+
+import Pattern2Observer.interfaces.DisplayElement;
+import Pattern2Observer.interfaces.Observer;
+import Pattern2Observer.interfaces.Subject;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+    private Subject weatherData;
+
+    private float currentPressure = 29.92f;
+    private float lastPressure = 0.0f;
+
+
+    public ForecastDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+
+    @Override
+    public void display() {
+        System.out.print("Forecast: ");
+        if (currentPressure > lastPressure)
+            System.out.println("Improving weather on the way!");
+        else if (currentPressure == lastPressure)
+            System.out.println("More of the same");
+        else if (currentPressure < lastPressure)
+            System.out.println("Watch out for cooler, rainy weather");
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        lastPressure = currentPressure;
+        currentPressure = pressure;
+        display();
+    }
+}
