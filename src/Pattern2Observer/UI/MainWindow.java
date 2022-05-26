323653e2b1da62b369c5c0d8dc2117a7ebb89900
Your Name <vladimiruppicki@gmail.com>
2022-05-26 22:48:55 +0300

    Observer: panel was added to window Listener was added to combobox

--- src/Pattern2Observer/UI/MainWindow.java
+++ src/Pattern2Observer/UI/MainWindow.java
@@ -2,6 +2,8 @@ package Pattern2Observer.UI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
     String[] items = {
@@ -10,6 +12,15 @@ public class MainWindow extends JFrame {
             "el3",
             "el4"
     };
+    private Dimension labelDimension = new Dimension(240, 130);
+    private Dimension comboBoxDimension = new Dimension(200, 20);
+
+    private JComboBox<String> comboBox;
+    private JPanel weatherPanel;
+    private JLabel weatherLabel;
+
+
+
     public static void main(String[] args) {
         MainWindow window = new MainWindow();
 
@@ -25,15 +36,40 @@ public class MainWindow extends JFrame {
 
         Container content = getContentPane();
 
-        JComboBox<String> comboBox = new JComboBox<>(items);
+        comboBox = new JComboBox<>(items);
         comboBox.setAlignmentX(LEFT_ALIGNMENT);
-        content.add(comboBox);
+        comboBox.setPreferredSize(comboBoxDimension);
+        comboBox.addActionListener(new AListener());
+
+        content.add(comboBox, BorderLayout.NORTH);
 
 
-        JTable table = new JTable();
+        weatherPanel = new JPanel();
+        weatherPanel.setBorder(BorderFactory.createTitledBorder(
+                (String) comboBox.getSelectedItem()));
+
+        weatherLabel = new JLabel();
+        weatherLabel.setPreferredSize(labelDimension);
+        weatherPanel.add(weatherLabel);
+
+        content.add(weatherPanel, BorderLayout.CENTER);
 
 
-        setPreferredSize(new Dimension(240, 130));
+
         pack();
     }
+
+
+
+    private class AListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String item = (String) comboBox.getSelectedItem();
+            weatherPanel.setBorder(BorderFactory.createTitledBorder(
+                    (String) comboBox.getSelectedItem()));
+        }
+    }
+
 }
+
